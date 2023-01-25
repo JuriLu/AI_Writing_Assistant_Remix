93ffa2cb703704125015d29d4c62a7bc17e3c0f0
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2023-01-25 10:11:35 +0000

    Login + Logout

--- aiwriting/app/routes/index.tsx
+++ aiwriting/app/routes/index.tsx
@@ -1,15 +1,38 @@
-import { Link } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
 
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <div>
-      <h1 className="text-slate-100">
-        This is the index page
-      </h1>
-      <p className="text-slate-300">Welcome {user?.email}</p>
+    <div className="flex item-center justify-center min-h-screen ">
+     <header className=" max-w-3x1">
+         <h1 className="text-slate-100">
+           This is the index page
+         </h1>
+         { 
+         user ? (
+           <div className="text-slate-200 mt-4">
+             <p >Welcome {user?.email}</p>
+             <Form action="/logout" method="post">
+               <button 
+               type="submit"
+               className=" rounded bg-slate-500 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-500"
+                >Log Out</button>         
+             </Form>      
+           </div>
+         ): (
+           <div className="flex gap-5 text-slate-200 mt-4">
+             <Link 
+             className=" rounded bg-slate-500 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-500"
+             to='/join'>Join</Link>       
+             <Link 
+             className=" rounded bg-slate-500 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-500"
+             to='/login'>Login</Link>       
+           </div>
+         )
+         }
+      </header>
     </div>
   );
 }
