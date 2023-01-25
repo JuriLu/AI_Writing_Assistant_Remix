8764496c90bd7c508fee2315860fd65a342664d7
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2023-01-25 10:11:20 +0000

    prisma seed

--- aiwriting/prisma/seed.ts
+++ aiwriting/prisma/seed.ts
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "lubonjajurgen1@gmail.com";
+  const email = "jurgen.lubonja@email.com";
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
